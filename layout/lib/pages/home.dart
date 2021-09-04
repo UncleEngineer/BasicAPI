5845b69c669a18f98f630d318f46da28de283a7b
Uncle Engineer <loong.wissawakorn@gmail.com>
2021-09-04 21:00:23 +0700

    Update ฟังชั่นดึงข้อมูล API จาก github

--- layout/lib/pages/home.dart
+++ layout/lib/pages/home.dart
@@ -2,6 +2,9 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:layout/pages/detail.dart';
 
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
 class HomePage extends StatefulWidget {
   // const HomePage({ Key? key }) : super(key: key);
 
@@ -18,17 +21,18 @@ class _HomePageState extends State<HomePage> {
         ),
         body: Padding(
           padding: EdgeInsets.all(20),
-          child: FutureBuilder( builder: (context, snapshot) {
-              var data = json.decode(snapshot.data.toString()); // [{คอมพิวเตอร์คืออะไร...},{},{},{}]
+          child: FutureBuilder( builder: (context, AsyncSnapshot snapshot) {
+              // var data = json.decode(snapshot.data.toString()); // [{คอมพิวเตอร์คืออะไร...},{},{},{}]
               return ListView.builder(
                 itemBuilder: (BuildContext context, int index) {
-                    return MyBox(data[index]['title'], data[index]['subtitle'], data[index]['image_url'],data[index]['detail']);
+                    return MyBox(snapshot.data[index]['title'], snapshot.data[index]['subtitle'], snapshot.data[index]['image_url'],snapshot.data[index]['detail']);
                 
                 },
-                itemCount: data.length, );
+                itemCount: snapshot.data.length, );
 
           },
-          future: DefaultAssetBundle.of(context).loadString('assets/data.json'),
+          future: getData(),
+          // future: DefaultAssetBundle.of(context).loadString('assets/data.json'),
 
 
           )
@@ -79,4 +83,15 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
+
+  Future getData() async {
+    // https://raw.githubusercontent.com/UncleEngineer/BasicAPI/main/data.json
+    var url = Uri.https('raw.githubusercontent.com','/UncleEngineer/BasicAPI/main/data.json');
+    var response = await http.get(url);
+    var result = json.decode(response.body);
+    return result;
+  }
+
+
 }
